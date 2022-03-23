8be014660dc3998d7cdd91cec675b572e96fcd66
gmeruvia <gmeruviarojas@gmail.com>
2022-03-22 20:23:03 -0400

    PROJECT CREATED

---
+++ src/main/java/com/laqfagal/fact/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.laqfagal.fact;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FactApplication.class);
+	}
+
+}
